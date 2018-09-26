c3c1d2e6053f1f10f8dd12ebc069abbcf60e9e68
soxueren <250029975@qq.com>
2018-09-26 19:19:56 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -17,17 +17,29 @@ if [ -z "$VIEWER_PASSWD" ]; then
 VIEWER_PASSWD=Test@402
 echo "default set VIEWER_PASSWD is Test@402"
 fi
+
+if [ -z "$VNC_DISPLAY" ]; then
+VNC_DISPLAY=1
+echo "default set VNC_DISPLAY is 1"
+fi
+
+
+if [ -z "$DCV_DISPLAY" ]; then
+DCV_DISPLAY=1
+echo "default set DCV_DISPLAY is 1"
+fi
+
+
 #start vncserver
 if [ ! -f "$startfile" ]; then
 echo "first start vncserver,please set your env: ROOT_PASSWD and VIEWER_PASSWD for dcvserver"
 # expect first vncserver start,set password
 /init_commond.exp  "$ROOT_PASSWD" "$VIEWER_PASSWD"
-vncserver -kill :1 && cp -af /tmp/xstartup /root/.vnc/xstartup &&  vncserver :1
- # && tail -f /vncserver.log
-else
-vncserver -kill :1 && cp -af /tmp/xstartup /root/.vnc/xstartup &&  vncserver :1
 fi
-echo "vncserver is running"
+
+vncserver -kill :1 && cp -af /tmp/xstartup /root/.vnc/xstartup &&  vncserver :1
+echo "vncserver is running  in :$VNC_DISPLAY"
+
 
 if [ -z "$1" ] || [[ $1 =~ -h|--help ]]; then
 echo "
